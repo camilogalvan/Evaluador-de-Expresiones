f0a7e94c9d87e549b5824d7d166e1cbed52e66ae
JUAN SEBASTIAN SANCHEZ PRADA <juansebastiansp@ufps.edu.co>
2019-10-25 23:34:41 +0000

    Update TestEvaluador.java

--- src/ufps/vistas/TestEvaluador.java
+++ src/ufps/vistas/TestEvaluador.java
@@ -6,7 +6,6 @@
 package ufps.vistas;
 
 import Evaluador.EvaluadorExpresiones;
-import Evaluador.Expresion;
 
 /**
  *
@@ -15,24 +14,9 @@ import Evaluador.Expresion;
 public class TestEvaluador {
 
     public static void main(String[] args) {
-//        String url = "https://gitlab.com/madarme/archivos-persistencia/raw/master/expresiones/expresion.txt.txt";
-//        EvaluadorExpresiones ev=new EvaluadorExpresiones(url);
-////        System.out.println(ev);
-//        System.out.println(ev.evaluarExpresion());
-        Expresion e = new Expresion("a,+,b,*,c,+,d,/,e");
-//        System.out.println(e.expresionValida());
-        
-//        System.out.println(e.getPosfijo("a,+,b,*,c,+,d,/,e"));
-//        System.out.println(e.getPosfijo("a,+,b,*,c"));
-//        System.out.println(e.getPosfijo("a,+,b,-,c"));
-//        System.out.println(e.getPosfijo("123,*,(,12,+,3,),+,4,*,(,6,+,2,)"));
-//        System.out.println(e.getPosfijo("10,+,12,*,12"));
-//        System.out.println(e.getPosfijo("10,+,(,7,+,12,)"));
-//        System.out.println(e.getPosfijo("10,/,6"));
-//        System.out.println(e.getPosfijo("(,(,(,5,+,9,),*,2,),+,(,6,*,5,),)"));
-//        System.out.println(e.getPosfijo("25,*,6,+,4,+,5,*,7"));
-        System.out.println(e.getPosfijo("25,*,6,+,(,6,+,8,)"));
-        
-//        
+        String url = "https://gitlab.com/juansebastiansp/archivos-persistencia/raw/bd822e4de11efd96f8daaf88518bc2a93e19b1cf/expresiones/expresion.txt.txt";
+        EvaluadorExpresiones ev=new EvaluadorExpresiones(url);
+        System.out.println(ev);
+        System.out.println(ev.evaluarExpresion());
     }
 }
