c04a31538d5b9c95b7e5a63e2be0fca61c951464
JUAN SEBASTIAN SANCHEZ PRADA <juansebastiansp@ufps.edu.co>
2019-10-25 23:33:02 +0000

    Update Expresion.java

--- src/Evaluador/Expresion.java
+++ src/Evaluador/Expresion.java
@@ -33,34 +33,32 @@ public class Expresion {
         String msg = "";
         cadena += v[0];
         for (int i = 1; i < v.length; i++) {
-//            if (v[i].equals("(") && !op.equals("(") && !esOperador(op)) {
-//                cadena += ",*," + v[i];
-//                op = "" + v[i];
-//            } else if (op.equals(")") && !esOperador((String) v[i])) {
-//                cadena += ",*," + v[i];
-//            } else {
-//            cadena += "," + v[i];
-//                op = "" + v[i];
-//            }
             cadena += "," + v[i];
         }
         if (validarParentesis(cadena)) {
-            msg += cadena + "\n";
-            msg += "Posfijo: " + getPosfijo(cadena) + "\n";
+            msg += "Posfijo: " + getPosfijo() + "\n";
+            msg += "Prefijo: " + getPrefijo(voltearCadenaPre(getPosfijo())) + "\n";
+            msg += "Resultado: " + getEvaluarPosfijo() + "\n";
+        } else {
+            msg += "Error de expresión: " + cadena + "\n";
         }
         return msg;
     }
 
-    private boolean verificarOperadores(String c) {
-        char[] v = c.toCharArray();
-        char letra = ' ';
+    private String voltearCadenaPre(String c) {
+        String[] v = c.split(",");
+        String msg = "";
+        msg += v[v.length - 1];
 
-        return false;
+        for (int i = v.length - 2; i >= 0; i--) {
+            msg += "," + v[i];
+        }
+        return msg;
     }
 
     private boolean esOperador(String l) {
-        return (l.equals('+') || l.equals('*') || l.equals('-')
-                || l.equals('/'));
+        return (l.equals("+") || l.equals("*") || l.equals("-")
+                || l.equals("/"));
     }
 
     private boolean validarParentesis(String c) {
@@ -80,115 +78,180 @@ public class Expresion {
         return p1.esVacia();
     }
 
-    public String getPrefijo() {
-        return "";
-    }
-
-    public String getPosfijo(String c) {
-        String[] v = c.split(",");
+    public String getPosfijo() {
         Pila<String> p1 = new Pila<>();
-        Pila<String> p2 = new Pila<>();
         ListaCD<String> posfijo = new ListaCD<>();
-        int tamanio = 0;
 
-        for (String dato : v) {
-            // 6+5-3*7/4
-            //cola-> 6,5,+,3,7,
-            //pila-> +,
-            //entra-> ('-') -> ('-' <= '+') -> cola->'+' y apila '-'
-            //pila-> -,*,/
-
+        for (String dato : this.expresiones) {
             switch (dato) {
                 case "+":
                 case "-":
                 case "*":
                 case "/":
-                    if (p2.esVacia()) {
+                    if (p1.esVacia() || p1.getTope().equals("(")) {
                         p1.apilar(dato);
-                        tamanio++;
                     } else {
-                        p2.apilar(dato);
+                        while (!p1.esVacia()) {
+                            if (getPrioridadExpresion(dato) > getPrioridadPila(p1.getTope())) {
+                                p1.apilar(dato);
+                                break;
+                            } else {
+                                posfijo.insertarAlFinal(p1.desapilar());
+                                if (p1.esVacia()) {
+                                    p1.apilar(dato);
+                                    break;
+                                }
+                            }
+                        }
                     }
                     break;
                 case "(":
-                    p2.apilar(dato);
+                    p1.apilar(dato);
                     break;
                 case ")":
-                    while (!p2.esVacia()) {
-                        String desapilar = p2.desapilar();
-                        if (desapilar.equals("(")) {
-                            break;
-                        }
-                        if (!p2.esVacia()) {
-                            String des = p2.desapilar();
-                            if (!des.equals("(")) {
-                                if (getPrioridad(desapilar) <= getPrioridad(des)) {
-                                    posfijo.insertarAlFinal(des);
-                                    posfijo.insertarAlFinal(desapilar);
-                                } else {
-                                    posfijo.insertarAlFinal(desapilar);
-                                    posfijo.insertarAlFinal(des);
-                                }
-                            } else {
-                                posfijo.insertarAlFinal(desapilar);
-                                break;
-                            }
-                        } else {
-                            posfijo.insertarAlFinal(desapilar);
-                        }
+                    while (!p1.getTope().equals("(")) {
+                        posfijo.insertarAlFinal(p1.desapilar());
                     }
+                    p1.desapilar();
                     break;
                 default:
-                    
-                    if (tamanio > 1) {
-                        String tope = p1.desapilar();
-                        String des = p1.desapilar();
-                        tamanio -= 2;
-                        if (getPrioridad(tope) <= getPrioridad(des)) {
-                            posfijo.insertarAlFinal(des);
-                            posfijo.insertarAlFinal(dato);
-                            posfijo.insertarAlFinal(tope);
-                        } else {
-                            posfijo.insertarAlFinal(dato);
-                            posfijo.insertarAlFinal(tope);
-                            posfijo.insertarAlFinal(des);
-                        }
-                    } else {
-                        posfijo.insertarAlFinal(dato);
-                    }
+                    posfijo.insertarAlFinal(dato);
                     break;
             }
         }
-
         while (!p1.esVacia()) {
             posfijo.insertarAlFinal(p1.desapilar());
         }
+        return getPostfYPref(posfijo);
+    }
 
+    public String getPrefijo(String c) {
+        String[] v = c.split(",");
+        Pila<String> p1 = new Pila<>();
+        ListaCD<String> prefijo = new ListaCD<>();
+
+        for (String dato : v) {
+            switch (dato) {
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                    if (p1.esVacia() || p1.getTope().equals("(")) {
+                        p1.apilar(dato);
+                    } else {
+                        while (!p1.esVacia()) {
+                            if (getPrioridadExpresion(dato) > getPrioridadPila(p1.getTope())) {
+                                p1.apilar(dato);
+                                break;
+                            } else {
+                                prefijo.insertarAlInicio(p1.desapilar());
+                                if (p1.esVacia()) {
+                                    p1.apilar(dato);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    break;
+                case ")":
+                    p1.apilar(dato);
+                    break;
+                case "(":
+                    while (!p1.getTope().equals(")")) {
+                        prefijo.insertarAlInicio(p1.desapilar());
+                    }
+                    p1.desapilar();
+                    break;
+                default:
+                    prefijo.insertarAlInicio(dato);
+                    break;
+            }
+        }
+        while (!p1.esVacia()) {
+            prefijo.insertarAlInicio(p1.desapilar());
+        }
+        return getPostfYPref(prefijo);
+    }
+
+    private String getPostfYPref(ListaCD<String> l) {
         String pos = "";
-        for (String dato : posfijo) {
+        for (String dato : l) {
             pos += dato + ",";
         }
         return pos;
     }
-
+    
     public float getEvaluarPosfijo() {
-        return 0.0f;
+        String[] v = this.getPosfijo().split(",");
+        Pila<String> p1 = new Pila<>();
+        float op1;
+        float op2;
+        float resultado;
+
+        for (String dato : v) {
+            if (!esOperador(dato)) {
+                p1.apilar(dato);
+            } else {
+                op2 = Float.parseFloat(p1.desapilar());
+                op1 = Float.parseFloat(p1.desapilar());
+                switch (dato) {
+                    case "+":
+                        resultado = op1 + op2;
+                        p1.apilar(Float.toString(resultado));
+                        break;
+                    case "-":
+                        resultado = op1 - op2;
+                        p1.apilar(Float.toString(resultado));
+                        break;
+                    case "*":
+                        resultado = op1 * op2;
+                        p1.apilar(Float.toString(resultado));
+                        break;
+                    case "/":
+                        resultado = op1 / op2;
+                        p1.apilar(Float.toString(resultado));
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        return Float.parseFloat(p1.desapilar());
     }
 
-    private int getPrioridad(String dato) {
+    private int getPrioridadExpresion(String dato) {
         int prioridad = 6;
 
         if (dato.equals("(") || dato.equals(")")) {
-            prioridad = 6;
-        }
-        
-        if (dato.equals("*") || dato.equals("/")) {
             prioridad = 5;
+            return prioridad;
+        }
+        if (dato.equals("*") || dato.equals("/")) {
+            prioridad = 4;
+            return prioridad;
         }
         if (dato.equals("+") || dato.equals("-")) {
-            prioridad = 4;
+            prioridad = 3;
+            return prioridad;
         }
+        return prioridad;
+    }
 
+    private int getPrioridadPila(String dato) {
+        int prioridad = 5;
+
+        if (dato.equals("*") || dato.equals("/")) {
+            prioridad = 4;
+            return prioridad;
+        }
+        if (dato.equals("+") || dato.equals("-")) {
+            prioridad = 3;
+            return prioridad;
+        }
+        if (dato.equals("(") || dato.equals(")")) {
+            prioridad = 0;
+            return prioridad;
+        }
         return prioridad;
     }
 
